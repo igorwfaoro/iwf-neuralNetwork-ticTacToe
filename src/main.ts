95eb973508ba64446619154c3a8fa7ee7986300a
Igor Wilian Faoro <igor.faoro17@gmail.com>
2024-04-30 22:54:19 -0300

    feat: create neural network

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createGame } from './game';
 import { createNeuralNetwork } from './neural-network';
+import { GameState } from './types/game-state';
+import { PlayerNames } from './types/player';
 
 const run = () => {
   const neuralNetwork = createNeuralNetwork({
@@ -9,7 +11,25 @@ const run = () => {
   });
 
   while (true) {
-    createGame().run();
+    const game = createGame();
+
+    console.log("Let's play Tic Tack Toe!");
+
+    game.printBoard();
+
+    let gameState: GameState = { isOver: false };
+    while (!gameState.isOver) {
+      game.makeMovement();
+      game.printBoard();
+
+      gameState = game.state();
+    }
+
+    console.log('Game over!');
+    const winner = gameState.winner;
+    if (winner) console.log(`The winner is ${PlayerNames[winner]}`);
+
+    console.log('-----------------\n');
   }
 };
 
